ee157ef058ecddb02211f9727bb05b9a6da89f38
alexandre-aco-code <77577613+alexandre-aco-code@users.noreply.github.com>
2022-07-21 10:30:51 +0200

    100% and cleaner

--- puzzles_moyens/shadows_of_the_knight_episode_1.py
+++ puzzles_moyens/shadows_of_the_knight_episode_1.py
@@ -8,7 +8,7 @@ import math
 # h: height of the building.
 w, h = [int(i) for i in input().split()]
 n = int(input())  # maximum number of turns before game over.
-x0, y0 = [int(i) for i in input().split()]
+x0, y0 = [int(i) for i in input().split()] # start position
 
 x = x0
 y = y0
@@ -19,13 +19,8 @@ y_min = 0
 x_max = w
 y_max = h
 
-
-
 while True:
     bomb_dir = input()  # the direction of the bombs
-
-    #if "R" in bomb_dir:
-    #   print("test")
 
     if "R" in bomb_dir:
         x_min = x
@@ -42,8 +37,4 @@ while True:
     x = (x_min + x_max)//2
     y = (y_min + y_max)//2
 
-
     print(x,y)
-    #print(0,0)
-
-print("Debug messages...", file=sys.stderr, flush=True)
