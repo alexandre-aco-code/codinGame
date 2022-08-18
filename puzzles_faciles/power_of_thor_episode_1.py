b0f305ac19eae38504f159d6a0eab10afa511ad5
alexandre-aco-code <77577613+alexandre-aco-code@users.noreply.github.com>
2022-08-18 11:25:13 +0200

    100% and cleaner

--- puzzles_faciles/power_of_thor_episode_1.py
+++ puzzles_faciles/power_of_thor_episode_1.py
@@ -1,67 +1,25 @@
-import sys
-import math
-
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
-# ---
-# Hint: You can use the debug stream to print initialTX and initialTY, if Thor seems not follow your orders.
-
-# light_x: the X position of the light of power
-# light_y: the Y position of the light of power
-# initial_tx: Thor's starting X position
-# initial_ty: Thor's starting Y position
-light_x, light_y, initial_tx, initial_ty = [int(i) for i in input().split()]
+# LX: the X position of the light of power
+# LY: the Y position of the light of power
+# TX: Thor's starting X position
+# TY: Thor's starting Y position
+LX, LY, TX, TY = [int(i) for i in input().split()]
 
 # game loop
 while True:
     remaining_turns = int(input())  # The remaining amount of turns Thor can move. Do not remove this line.
-
-    direction = ""
-
-    if initial_tx < light_x: 
-        initial_tx += 1
+    d = ""
+    if TY < LY: 
+        TY += 1
+        d = "S"
+    elif TY > LY :
+        TY -= 1
+        d = "N"
         
-        if initial_ty < light_y: 
-            initial_ty += 1
-            direction = "SE"
+    if TX < LX: 
+        TX += 1
+        d += "E"
+    elif TX > LX:
+        TX -= 1
+        d += "W"
 
-        elif initial_ty > light_y:
-            initial_ty -= 1
-            direction = "NE"
-
-        else:
-            direction = "E"
-
-    elif initial_tx > light_x:
-        initial_tx -= 1
-
-        if initial_ty < light_y: 
-            initial_ty += 1
-            direction = "SW"
-
-        elif initial_ty > light_y:
-            initial_ty -= 1
-            direction = "NW"
-
-        else:
-            direction = "W"
-
-    else :
-        if initial_ty < light_y: 
-            initial_ty += 1
-            direction = "S"
-
-        elif initial_ty > light_y:
-            initial_ty -= 1
-            direction = "N"
-
-        else : 
-            direction = "ARRIVE"
-
-    print(direction)
-        
-
-    # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
-
-    # A single line providing the move to be made: N NE E SE S SW W or NW
+    print(d)
