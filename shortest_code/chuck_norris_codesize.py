38cd2077ba302442de16bd7e2663ef17e86e5c73
alexandre-aco-code <77577613+alexandre-aco-code@users.noreply.github.com>
2022-08-18 16:17:22 +0200

    267

--- shortest_code/chuck_norris_codesize.py
+++ shortest_code/chuck_norris_codesize.py
@@ -1,10 +1,10 @@
-bt,ut,p="","",False
-for c in input():bt+="{0:07b}".format(ord(c))
-if len(bt) >= 1:
-    if bt[0]=="0":ut+="00 0"
-    else:ut+="0 0";p=True
-for i in range(1,len(bt)):
-    if bt[i]=="0" and p:ut+=" 00 0";p=False
-    elif bt[i]=="1" and not p:ut+=" 0 0";p=True
-    else:ut+="0"
-print(ut)
+a,b,p="","",False
+for c in input():a+="{0:07b}".format(ord(c))
+if len(a)>=1:
+    if a[0]=="0":b+="00 0"
+    else:b+="0 0";p=True
+for i in range(1,len(a)):
+    if a[i]=="0" and p:b+=" 00 0";p=False
+    elif a[i]=="1" and not p:b+=" 0 0";p=True
+    else:b+="0"
+print(b)
